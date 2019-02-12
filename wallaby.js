90a13a9452a2d03e6a51cf0bfe634ab4d87bb571
Simon McEnlly <simon.mcenlly@gmail.com>
2019-02-13 08:17:01 +1000

    Update wallaby.js

--- wallaby.js
+++ wallaby.js
@@ -1,20 +1,40 @@
-module.exports = function (wallaby) {
+module.exports = function(wallaby) {
+  process.env.VUE_CLI_BABEL_TRANSPILE_MODULES = true;
+
+  const compiler = wallaby.compilers.babel();
+
   return {
     files: [
+      'jest.config.js',
+      'package.json',
       'src/**/*.js?(x)',
+      'src/**/*.vue',
       '!src/**/*.spec.js?(x)'
     ],
-    tests: [
-      'test/**/*spec.js?(x)'
-    ],
+
+    tests: ['test/**/*spec.js?(x)'],
+
     env: {
       type: 'node',
       runner: 'node'
     },
+
     compilers: {
-      "**/*.js?(x)": wallaby.compilers.babel()
+      '**/*.js?(x)': compiler,
+      '**/*.vue': require('wallaby-vue-compiler')(compiler)
     },
+
+    preprocessors: {
+      '**/*.vue': file => require('vue-jest').process(file.content, file.path)
+    },
+
+    setup: function(wallaby) {
+      const jestConfig = require('./package').jest || require('./jest.config');
+      jestConfig.transform = {};
+      wallaby.testFramework.configure(jestConfig);
+    },
+
     testFramework: 'jest',
-    debug: true,
+    debug: true
   };
 };
